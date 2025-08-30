168ac3a507240f5c20add724ad841530ce434634
Mohamed Gaber <me@donn.website>
2025-08-30 06:21:26 +0300

    fix hacks, skip pypy for now

--- libparse/wrapper.cpp
+++ libparse/wrapper.cpp
@@ -15,12 +15,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#define private public
-#include "libparse.h"
-#undef private
+#include <sstream>
+#include <string>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+
+#include "libparse.h"
 
 #include "stdio_filebuf.h"
 
